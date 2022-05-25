3a2ee0c51def8cea2f4e31ac227a79458d85d9fb
summereuna <summereuna@gmail.com>
2022-05-25 18:32:37 +0900

    Fix: add displayName to email account user

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import AppRouter from "components/Router";
 import { useEffect, useState } from "react";
 //절대 경로(absolute import)
 //fbase에서 authService 가져오기(export로 내보냈기 때문에 {} 중괄호 쳐서 가져와야 함)
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
 
 function App() {
   //firebase가 프로그램을 초기화하길 기다리고 나서 isLoggedIn이 바뀌게 해야 한다.
@@ -17,10 +17,18 @@ function App() {
     const auth = getAuth();
     //유저 변화가 있는지 listen하기: onAuthStateChanged관찰자 사용
     //onAuthStateChanged은 콜백이 필요한데, 콜백은 user이다.
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, async (user) => {
       //console.log(user);
       //user가 있다면 로그인한 유저 정보 userObj에 업데이트, 로그아웃하면 null
-      user ? setUserObj(user) : setUserObj(null);
+      //user ? setUserObj(user) : setUserObj(null);
+      if (user) {
+        if (user.displayName === null) {
+          await updateProfile(user, { displayName: "유저" });
+        }
+        setUserObj(user);
+      } else {
+        setUserObj(null);
+      }
       //그러고 나서 초기화 시켜라
       setInit(true);
     });
